62dfc0449b72739b71935256ea8e11e310630449
nntcao <81784608+nntcao@users.noreply.github.com>
2024-04-01 08:51:50 -0500

    fix test script, upload mypython executable

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# PythonInterpreter
-Ordonez
+# COSC 4315 PythonInterpreter w/ Ordonez
+A toy interpreter for the Python language specifications. Includes integer variables, basic expression evaluation, control flow, and functions.
+
+To compile run the following in the main directory:
+`g++ -std=c++11 *.cpp -o mypython`
+
+Usage:
+`./mypython <file.py>`
+
+To compile and test the script, run:
+`./test.sh`
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ lf_result="./lf_result.txt"
 crlf_result="./crlf_result.txt"
 for testcase in "./testcases/in"*".py"; do
     output="./testcases/out"${testcase:14:2}".txt"
-    ./mypython.exe "$testcase" > "$lf_result"
+    ./mypython "$testcase" > "$lf_result"
     cat "$lf_result" | sed 's/\r$//' > "$crlf_result"
     if [ "$(md5sum < "$output")" = "$(md5sum < "$crlf_result")" ]; then
         echo "PASS "$testcase
